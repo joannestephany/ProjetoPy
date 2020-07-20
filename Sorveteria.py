043a4c822b8d6c789ef894329ed798a298896363
Porty-Eric <67772327+Porty-Eric@users.noreply.github.com>
2020-07-20 16:03:44 -0300

    Update Sorveteria.py

--- Sorveteria.py
+++ Sorveteria.py
@@ -1,7 +1,9 @@
 import os
 
+
 #                 FUNÇÃO PARA ESCOLHER O CARDAPIO - TAMBÉM PRIMEIRA TELA
 def escolhaCardapio():
+    global card
     card = 1
     while card != 0:
         print("ESCOLHA SEU CARDÁPIO\n")
@@ -15,14 +17,14 @@ def escolhaCardapio():
         elif card == 2:
             tamanhosPreco(card)
         elif card == 0:
-            print("FIM DO PEDIDO!!!") #tem que chamar a função aqui que calcula tudo
+            print("FIM DO PEDIDO!!!")  # tem que chamar a função aqui que calcula tudo
             break
         else:
             print('Valor inválido, escolha outra opção.')
 
 
-#                FUNÇÃO QUE MOSTRA MENU SORVETE - INCOMPLETA                  
-def cardSorvete(preco):       #Após escolher o sabor o pedido do usuário vai par o "carrinho com o sabor e o preço"
+#                FUNÇÃO QUE MOSTRA MENU SORVETE - INCOMPLETA
+def cardSorvete(preco):  # Após escolher o sabor o pedido do usuário vai par o "carrinho com o sabor e o preço"
     sabores = list()
     sabores.append('1 - Chocolate')
     sabores.append('2 - Baunilha')
@@ -30,60 +32,106 @@ def cardSorvete(preco):       #Após escolher o sabor o pedido do usuário vai p
     sabores.append('4 - Morango')
     print(sabores)
     return sabores
+
+
 #               FUNÇÃO QUE MOSTRA MENU ACAI - INCOMPLETA
-def cardAcai(preco):         #Após escolher o sabor o pedido do usuário vai par o "carrinho com o sabor e o preço"
+def cardAcai(tam):  # Após escolher o sabor o pedido do usuário vai par o "carrinho com o sabor e o preço"
     complementos = list()
     complementos.append('Castanha')
     complementos.append('Amendoim')
     complementos.append('Banana')
     complementos.append('Achocolatado')
-    return complementos
+    print("Nossos complementos são: {}".format(complementos))
     seusComplementos = []
-    while len(seusComplementos)< tam+2:
-        print("Escolha seu {}º complemento.".format(len(seusComplementos)+1))
+    print(tam)
+    while len(seusComplementos) < tam+2:
+        print("Escolha seu {}º complemento.".format(len(seusComplementos) + 1))
         nomeDoComp = input()
         seusComplementos.append(nomeDoComp)
     print("Seus complementos foram: {}".format(seusComplementos))
 
+
 def fimPedido():  # nessa função quando for encerrar vai fazer o calculo dos itens pedidos
     print("fim")  # só para ter alguma coisa
 
+
 #                   FUNÇAO QUE ESCOLHE O TAMANHO DO SORVETE/ACAI (CHAMA FUNCAO MENU SORVETE/ACAI)
-def tamanhosPreco(opt): 
+def tamanhosPreco(opt):
     print('DIGITE O NÚMERO CORRESPONDENTE: ')
     print('1 - PEQUENO = 200ml = 4,00 R$ ')
     print('2 - MEDIO = 400ml = 7,00 R$')
     print('3 - GRANDE = 600ml = 10,00 R$')
     print('0 - VOLTAR')
-    tam = 1
+    condicao = 1
     preco = 0
-    while tam != -1:
+    while condicao != -1:
         tam = int(input('Escolha o tamanho: '))
         if tam == 1 or tam == 2 or tam == 3 or tam == 0:
-        if tam == 1: 
-            preco = 4
-            tam = -1
-        elif tam == 2:
-            preco = 7
-            tam = -1
-        elif tam == 3:
-            preco = 10
-            tam = -1
-        elif tam == 0:
-            return 
-        else:
-            print('Opção inválida.')
-    if opt == 1: 
+            if tam == 1:
+                preco = 4
+                condicao = -1
+            elif tam == 2:
+                preco = 7
+                condicao = -1
+            elif tam == 3:
+                preco = 10
+                condicao = -1
+            elif tam == 0:
+                return
+            else:
+                print('Opção inválida.')
+    if opt == 1:
         cardSorvete(preco)
     else:
-        cardAcai(preco)
+        cardAcai(tam)
+
+
 '''
 PARAMOS NESSA FUNÇAO
 '''
-print('BEM VINDO À SORVEETERIA PY \n')   
+print('BEM VINDO À SORVEETERIA PY \n')
 escolhaCardapio()
-#card = escolhaCardapio()
-#openCardapio() #tá dando erro aqui
-#tem que chamar todas as outras funções
+# card = escolhaCardapio()
+
+def validarCPF(cpf):
+    if cpf.count(cpf[1]) == 11:
+        print('CPF inválido, confira os dados.')
+    elif len(cpf) != 11:
+        print('CPF inválido, confira os dados.')
+    else:
+        soma1 = 0
+        cont = 10
+        for i in range(2, 11):
+            soma1 += (int(cpf[i - 2]) * cont)
+            cont -= 1
+
+        resto = (soma1 * 10) % 11
+        if resto != 10 and resto != int(cpf[-2]):
+            print('CPF inválido, confira os dados.')
+        else:
+            soma2 = 0
+            cont = 11
+            for x in range(2, 12):
+                soma2 += (int(cpf[x - 2]) * cont)
+                cont -= 1
+
+            resto = (soma2 * 10) % 11
+            if resto != int(cpf[-1]):
+                print('CPF inválido, confira os dados.')
+            else:
+                print('CPF Válido!')
+                return True
 
 
+def menuCPF():
+    flag = False
+    while flag != True:
+        opt = input(f'Digite o cpf ou Apenas 0 para cancelar: ')
+        if int(opt) != 0:
+            flag = validarCPF(opt)
+        else:
+            print('Operação cancelada.')
+            flag = True
+
+
+menuCPF()
