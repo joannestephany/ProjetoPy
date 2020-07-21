8d76c32ec63dee4235c5006e6bb8cc93601258a6
Joanne Stephany <58868281+joannestephany@users.noreply.github.com>
2020-07-20 22:01:49 -0300

    Atualização feita em reunião
    falta principalmente:
    fazer o carrinho
    usar a função cpf
    imprimir nota
    
    a editar: 
    entrada do tipo de complemento ou sabor para numero

--- Sorveteria.py
+++ Sorveteria.py
@@ -1,4 +1,7 @@
 import os
+ 
+def limparTela():
+  os.system('cls')
 
 #                 FUNÇÃO PARA ESCOLHER O CARDAPIO - TAMBÉM PRIMEIRA TELA
 def escolhaCardapio():
@@ -9,7 +12,7 @@ def escolhaCardapio():
         print(" > Digite 1 para abrir o caradápio de Sorvete")
         print(" > Digite 2 para abrir o cardápio de Açai")
         print(" > Digite 0 Finalizar pedido.")
-
+ 
         card = int(input(" Escolha: "))
         if card == 1:
             tamanhosPreco(card)
@@ -17,47 +20,60 @@ def escolhaCardapio():
             tamanhosPreco(card)
         elif card == 0:
             print("FIM DO PEDIDO!!!")  # tem que chamar a função aqui que calcula tudo
+            #nota()
             break
         else:
             print('Valor inválido, escolha outra opção.')
-
-
+ 
+ 
 def cardSorvete(tam):  # Após escolher o sabor o pedido do usuário vai par o "carrinho com o sabor e o preço"
-    sabores = list()
-    sabores.append('Chocolate')
-    sabores.append('Baunilha')
-    sabores.append('Flocos')
-    sabores.append('Morango')
+    sabores = ('Chocolate','Baunilha','Flocos','Morango')
     print("Nossos sabores são: {}".format(sabores))
+
     seusSabores = []
     while len(seusSabores) < tam:
-        print("Escolha seu {}º sabor.".format(len(seusSabores) + 1))
+        print("Escolha seu {}º sabor.:(escreva) ".format(len(seusSabores) + 1))
         nomeDoSabor = input()
         seusSabores.append(nomeDoSabor)
     print("Seus sabores foram: {}".format(seusSabores))
-
-
+    
+ 
 def cardAcai(tam):  # Após escolher o sabor o pedido do usuário vai par o "carrinho com o sabor e o preço"
-    complementos = list()
-    complementos.append('Castanha')
-    complementos.append('Amendoim')
-    complementos.append('Banana')
-    complementos.append('Achocolatado')
+    complementos =('Catanha','Amendoim','Banana','Achocolatado','Leite em pó')
     print("Nossos complementos são: {}".format(complementos))
+
     seusComplementos = []
     print("Você tem direito a {} complemento(s)".format(tam))
     while len(seusComplementos) < tam:
-
-        print("Escolha seu {}º complemento.".format(len(seusComplementos) + 1))
+ 
+        print("Escolha seu {}º complemento.:(escreva) ".format(len(seusComplementos) + 1))
         nomeDoComp = input()
         seusComplementos.append(nomeDoComp)
+        
     print("Seus complementos foram: {}".format(seusComplementos))
 
+ 
+def nota(tam):
+    print(nomeCliente)
+    print('O preço a ser pago é R${:.2f}.'.format(preco))
+    cpfnota=input('Deseja CPF na nota? S/N').upper
+    if cpfnota == 'S':
+      print('arroz')
+      #função do cpf
+    elif cpfnota == 'N':
+      valorpago = float(input('Digite o valor pago: '))
+      if valorpago == preco:
+            print('PEDIDO ENCERRADO')
+      elif valorpago != preco:
+        if valorpago > preco:
+              troco = valorpago-preco
+              print(troco)
+        else:
+              falta = preco-valorpago
+              print(falta)
 
-def fimPedido():  # nessa função quando for encerrar vai fazer o calculo dos itens pedidos
-    print("fim")  # só para ter alguma coisa
 
-
+ 
 #                   FUNÇAO QUE ESCOLHE O TAMANHO DO SORVETE/ACAI (CHAMA FUNCAO MENU SORVETE/ACAI)
 def tamanhosPreco(opt):
     print('DIGITE O NÚMERO CORRESPONDENTE: ')
@@ -88,65 +104,14 @@ def tamanhosPreco(opt):
         cardSorvete(tam)
     else:
         cardAcai(tam)
-
-
+ 
+ 
 '''
 PARAMOS NESSA FUNÇAO
 '''
-print('BEM VINDO À SORVEETERIA PY \n')
+print('BEM VINDO À SORVETERIA PY \n')
 nomeCliente=input("Digite seu nome para começarmos: ")
 escolhaCardapio()
 print(nomeCliente)
-'''
-print(preco) vai mostrar o valor que a pessoa ter que pagar
-print(seusComplementos) vai mostrar os complementos  escolhidos
-print(seusSabores) vai mostrar os sabores escolihods
-!!!falta colocar condição caso a pessoa digite um sabor que não tenha (usando string).requisito
-!!!falta repetir o programa caso a pessoa queira pedir outro produto e acumular na variavel preco
-!!!falta usar valores boleanos(true/false).requisito
-!!!condicao aninhada/encadeada.requisito
-'''
-# card = escolhaCardapio()
+#nota()
 
-def validarCPF(cpf):
-    if cpf.count(cpf[1]) == 11:
-        print('CPF inválido, confira os dados.')
-    elif len(cpf) != 11:
-        print('CPF inválido, confira os dados.')
-    else:
-        soma1 = 0
-        cont = 10
-        for i in range(2, 11):
-            soma1 += (int(cpf[i - 2]) * cont)
-            cont -= 1
-
-        resto = (soma1 * 10) % 11
-        if resto != 10 and resto != int(cpf[-2]):
-            print('CPF inválido, confira os dados.')
-        else:
-            soma2 = 0
-            cont = 11
-            for x in range(2, 12):
-                soma2 += (int(cpf[x - 2]) * cont)
-                cont -= 1
-
-            resto = (soma2 * 10) % 11
-            if resto != int(cpf[-1]):
-                print('CPF inválido, confira os dados.')
-            else:
-                print('CPF Válido!')
-                return True
-
-
-def menuCPF():
-    flag = False
-    while flag != True:
-        opt = input(f'Digite o cpf ou Apenas 0 para cancelar: ')
-        if int(opt) != 0:
-            flag = validarCPF(opt)
-        else:
-            print('Operação cancelada.')
-            flag = True
-
-
-menuCPF()
